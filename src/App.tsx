9f7b88d978f653e0abec405b3b4e09262095fb51
Honeylene Insular <honeylene.insular@edu.sait.ca>
2026-02-23 00:40:46 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+// Homepage
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+    
+      <section>
+        <h2>Scan Receipt</h2>
+        <p>Take a photo, upload an image or drag and drop your receipt here</p>
+      </section>
+      <section>
+        {/* <h2>Contact Us</h2>
+        <p>This is the contact section.</p> */}
+      </section>
+      <footer>
+        <p>&copy; 2026 Receipt Keeper</p>
+      </footer>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
